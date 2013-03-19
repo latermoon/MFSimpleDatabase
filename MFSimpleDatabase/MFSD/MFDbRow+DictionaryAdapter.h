63c7f9f90df6d97c39b948635ffcf789b32668d8
latermoon <latermoon@qq.com>
2013-03-19 14:59:16 +0800

    Rename some files

---
+++ MFSimpleDatabase/MFSD/MFDbRow+DictionaryAdapter.h
@@ -0,0 +1,13 @@
+//
+//  MFDbRow+DictionaryAdapter.h
+//  MFSimpleDatabase
+//
+//  Created by Latermoon on 13-3-19.
+//  Copyright (c) 2013年 Latermoon.com. All rights reserved.
+//
+
+#import "MFDbRow.h"
+
+@interface MFDbRow (DictionaryAdapter)
+
+@end
---
+++ MFSimpleDatabase/MFSD/MFDbRow+DictionaryAdapter.m
@@ -0,0 +1,13 @@
+//
+//  MFDbRow+DictionaryAdapter.m
+//  MFSimpleDatabase
+//
+//  Created by Latermoon on 13-3-19.
+//  Copyright (c) 2013年 Latermoon.com. All rights reserved.
+//
+
+#import "MFDbRow+DictionaryAdapter.h"
+
+@implementation MFDbRow (DictionaryAdapter)
+
+@end
